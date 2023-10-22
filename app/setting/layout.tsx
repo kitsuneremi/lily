5ac895711c032ab8070ab9d9fb2f8cf5d334871f
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2023-10-22 16:55:44 +0700

    update setting layout
    update setting layout

--- app/setting/layout.tsx
+++ app/setting/layout.tsx
@@ -1,17 +1,6 @@
-import Navbar from "@/components/own/navbar"
-import Sidebar from '@/components/own/settingsidebar'
 export default function Layout({ children }: { children: React.ReactNode }) {
-
-
     return (
-        <div className="">
-            <Navbar />
-            <div className="flex pt-16 gap-4">
-                <Sidebar />
-                {children}
-            </div>
-        </div>
-
+        {children}
     )
 
 }
