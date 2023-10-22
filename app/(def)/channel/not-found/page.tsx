665d213dcd51ffe2541a3f7cbe7904feb77c9425
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2023-10-22 16:51:40 +0700

    channel not found page
    channel not found page

---
+++ app/(def)/channel/not-found/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+
+    return(
+        <div className="flex-1 h-full flex justify-center items-center">
+            <p>không tìm thấy kênh này, kiểm tra lại đường dẫn??</p>
+        </div>
+    )
+}
