20df027cf39479054df050d74d1d309dbfdbcc5c
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2023-10-25 16:48:08 +0700

    add padding
    add padding

--- components/own/studio/layout.tsx
+++ components/own/studio/layout.tsx
@@ -1,7 +1,7 @@
 export default function MyComponent({ children }: { children: React.ReactNode }) {
     return (
-        <>
+        <div className="px-5 flex-1 overflow-hidden">
             {children}
-        </>
+        </div>
     );
 }
