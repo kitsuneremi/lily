13ff8afad34a7176b470cde10cce084d9cc1930f
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2023-10-25 16:42:17 +0700

    create new route for comment
    create new route for new comment

---
+++ app/api/comments/create/route.ts
@@ -0,0 +1,37 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+type CommentBody = {
+    videoId: number,
+    accountId: number,
+    content: string
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+    const body: CommentBody = await req.json();
+    const { videoId, accountId, content } = body;
+    if (videoId && accountId && content) {
+        const cmt = await prisma.comment.create({
+            data: {
+                content: content,
+                accountId: accountId,
+                videoId: videoId,
+                referenceId: null,
+                status: 0
+            }
+        })
+
+        const listComment = await prisma.comment.findMany({
+            where: {
+                videoId: videoId
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        })
+
+        return new NextResponse(JSON.stringify(listComment), { status: 201 })
+    } else {
+        return new NextResponse(JSON.stringify(null), { status: 400 })
+    }
+}
