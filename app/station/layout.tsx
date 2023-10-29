5da8eb77943d0447a0c9dfe8220186130a6a65b9
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2023-10-30 01:05:57 +0700

    removed unnecessary layout

--- app/station/layout.tsx
+++ app/station/layout.tsx
@@ -1,10 +1,6 @@
 import Sidebar from "@/components/own/studio/sidebar";
 import Navbar from "@/components/own/studio/navbar";
-import prisma from "@/lib/prisma";
-import { getServerSession } from "next-auth";
 import { Metadata } from "next";
-import Layoutx from '@/components/own/studio/layout'
-
 // const channelDataFetch = async () => {
 //     const channels = await prisma.channels.findFirst({
 //         where: {
@@ -16,18 +12,22 @@ import Layoutx from '@/components/own/studio/layout'
 // }
 
 export const medata: Metadata = {
-    title: 'studio page',
-    description: 'studio page'
-}
+    title: "studio page",
+    description: "studio page",
+};
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
     return (
         <div className="w-screen h-screen">
             <Navbar />
             <div className="flex h-[calc(100vh-64px)]">
                 <Sidebar />
-                <Layoutx children={children} />
+                {children}
             </div>
         </div>
-    )
+    );
 }
