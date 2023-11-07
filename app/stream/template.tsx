8f8e795f6eb0dbb3b482a5617f928cdba67762e0
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2023-11-08 03:30:41 +0700

    change overflow-clip to hidden
    change overflow-clip to hidden

--- app/stream/template.tsx
+++ app/stream/template.tsx
@@ -4,11 +4,11 @@ import Navbar from "@/components/own/navbar";
 
 export default function Template({ children }: { children: React.ReactNode }) {
     return (
-        <div className="w-full flex flex-col h-screen">
+        <div className="w-full flex flex-col h-screen overflow-y-hidden">
             <div className="w-screen h-16 flex justify-between fixed top-0 left-0 px-3 lg:px-8 py-4 bg-white dark:bg-[#020817] z-10">
                 <Navbar />
             </div>
-            <div className="flex mt-16 h-[calc(100vh-64px)] overflow-y-clip">
+            <div className="flex mt-16 h-[calc(100vh-64px)]">
                 <Sidebar />
                 {children}
             </div>
