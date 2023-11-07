7fc33c35522822897d066ec9ed31db74d5a7401e
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2023-11-08 03:32:36 +0700

    new live create api route
    new live create api route

---
+++ app/api/live/create/route.ts
@@ -0,0 +1,31 @@
+import prisma from "@/lib/prisma";
+import { now } from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+
+    const body = await req.json();
+    console.log(body)
+    const channel = await prisma.channels.findUnique({
+        where: {
+            accountId: body.accountId
+        }
+    })
+    console.log(channel)
+    if (channel && channel.live) {
+        const newLive = await prisma.media.create({
+            data: {
+                channelId: channel.id,
+                des: body.des,
+                isLive: true,
+                title: body.title,
+                status: body.status,
+                mediaType: 1,
+                link: channel.tagName
+            }
+        })
+        return new Response(JSON.stringify(newLive), { status: 201 })
+    } else {
+        return new Response(JSON.stringify({ message: "no stream" }))
+    }
+}
