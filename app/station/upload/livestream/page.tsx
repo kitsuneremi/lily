e054cc43ba554b9e2d526c7b1b39b7f80ec56c07
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2023-11-08 03:36:16 +0700

    add channelData fetch from server to StreamPage
    add channelData fetch from server to StreamPage

--- app/station/upload/livestream/page.tsx
+++ app/station/upload/livestream/page.tsx
@@ -1,21 +1,38 @@
 import { Metadata } from "next"
 import StreamPage from '@/components/own/studio/upload/livestream/page'
-import { getServerSession } from "next-auth"
+import { Session, getServerSession } from "next-auth"
 import { authOptions } from "@/lib/auth"
+import { ChannelDataType } from "@/types/type"
+import { baseURL } from "@/lib/functional"
+import { redirect } from "next/navigation"
 
 export const metadata: Metadata = {
     title: 'thiết lập buổi phát trực tiếp',
     description: 'thiết lập buổi phát trực tiếp'
 }
 
+const fetchChannelData = async (session: Session) => {
+    const data = await fetch(`${baseURL}/api/channel/data?accountId=${session.user.id}`,{
+        next:{
+            revalidate: 5
+        }
+    })
+
+    return await data.json();
+}
+
 export default async function Page() {
 
     const session = await getServerSession(authOptions);
-
-    return (
-        <>
-            <StreamPage session={session}></StreamPage>
-        </>
-    )
+    if (session) {
+        const channelData: ChannelDataType = await fetchChannelData(session);
+        return (
+            <>
+                <StreamPage session={session} channelData={channelData}></StreamPage>
+            </>
+        )
+    } else {
+        redirect('/register')
+    }
 
 }
