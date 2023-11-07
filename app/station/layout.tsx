1b4965315682b9431ad9a75e69d8edeaabc277db
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2023-11-08 03:35:41 +0700

    redirect who doesn't have needed perrmission
    - redirect who unauthorized to register page
    - redirect who doesn't have channel to /regchannel

--- app/station/layout.tsx
+++ app/station/layout.tsx
@@ -1,6 +1,10 @@
 import Sidebar from "@/components/own/studio/sidebar";
 import Navbar from "@/components/own/studio/navbar";
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from 'next/navigation'
+import { baseURL } from "@/lib/functional";
 // const channelDataFetch = async () => {
 //     const channels = await prisma.channels.findFirst({
 //         where: {
@@ -16,18 +20,35 @@ export const medata: Metadata = {
     description: "studio page",
 };
 
+const fetchChannelData = async (id: number) => {
+    const data = await fetch(`${baseURL}/api/channel/data?accountId=${id}`);
+    return await data.json();
+}
+
 export default async function Layout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return (
-        <div className="w-screen h-screen">
-            <Navbar />
-            <div className="flex h-[calc(100vh-64px)]">
-                <Sidebar />
-                {children}
-            </div>
-        </div>
-    );
+
+    const session = await getServerSession(authOptions);
+    if (session) {
+        const channelData = await fetchChannelData(session?.user.id)
+        if (channelData) {
+            return (
+                <div className="w-screen h-screen">
+                    <Navbar />
+                    <div className="flex h-[calc(100vh-64px)]">
+                        <Sidebar />
+                        {children}
+                    </div>
+                </div>
+            );
+        } else {
+            return redirect('/regchannel')
+        }
+    } else {
+        return redirect('/register')
+    }
+
 }
