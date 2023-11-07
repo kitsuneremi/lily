65aa999832e689bedb215a1c51a321c05cff2b76
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2023-11-08 03:31:52 +0700

    now fetch data on server instead of client
    - now fetch data on server instead of client
    - update type base on database change

--- app/stream/[tag]/page.tsx
+++ app/stream/[tag]/page.tsx
@@ -1,35 +1,57 @@
 import { baseURL } from '@/lib/functional';
 import type { Metadata, ResolvingMetadata } from 'next'
-import { type VideoDataType, BigVideoDataType } from '@/types/type';
+import { type MediaDataType, BigVideoDataType, LiveData, ChannelDataType } from '@/types/type';
 import Watch from '@/indirect/watch/watch'
+import { redirect } from 'next/navigation'
+import StreamPage from '@/indirect/stream/stream'
 type Props = {
     params: { tag: string }
     searchParams: { [key: string]: string | string[] | undefined }
 }
 
-// export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+    // const data: LiveData = await fetchVideoData({ tag: params.tag });
+    const channelData: ChannelDataType = await fetchChannelData({ tagName: params.tag });
+    return {
+        title: `buổi phát trực tiếp của ${channelData.name}`,
+        description: `buổi phát trực tiếp của ${channelData.name}`
+    }
+}
 
-//     const link = params.link;
-//     const data: BigVideoDataType = await fetchVideoData({ link: link });
-//     return {
-//         title: data.videoData.title,
-//         description: data.videoData.des
-//     }
-// }
+const fetchVideoData = async ({ tag }: { tag: string }) => {
+    'use server'
+    const res = await fetch(`${baseURL}/api/live/data?tag=${tag}`, {
+        method: 'GET',
+        next: {
+            revalidate: 5
+        }
+    })
+    if (res.status == 400) {
+        return redirect(`/stream/not-found`)
+    } else if (res.status == 404) {
+        return redirect(`/stream/${tag}/no-stream`)
+    } else {
+        const data: MediaDataType = await res.json();
+        if (data.mediaType == 2) {
+            return redirect(`/watch/${data.link}`)
+        }
+        return data;
+    }
+}
 
-// const fetchVideoData = async ({ link }: { link: string }) => {
-//     'use server'
-//     const res = await fetch(`${baseURL}/api/video/data?link=${link}`, {
-//         method: 'GET',
-//         next: {
-//             revalidate: 5
-//         }
-//     })
-//     const data: BigVideoDataType = await res.json();
-//     return data;
-// }
+const fetchChannelData = async ({ tagName }: { tagName: string }) => {
+    const res = await fetch(`${baseURL}/api/channel/data?tagName=${tagName}`, {
+        method: 'GET',
+    })
+
+    const data: ChannelDataType = await res.json();
+    return data;
+}
 
 export default async ({ params, searchParams }: Props) => {
-    // const data = await fetchVideoData({ link: params.link })
-    return <div>watch live stream of {params.tag}</div>
-}
+    const data: MediaDataType = await fetchVideoData({ tag: params.tag })
+    const channelData: ChannelDataType = await fetchChannelData({ tagName: params.tag });
+    return (
+        <StreamPage streamData={data} channelData={channelData} />
+    )
+} 
