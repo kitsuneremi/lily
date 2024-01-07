24a5293114c45f21a55fc3a3d9e46d4f37d00262
lily <89727355+kitsuneremi@users.noreply.github.com>
2024-01-07 17:19:24 +0700

    Update template.tsx

--- app/(def)/template.tsx
+++ app/(def)/template.tsx
@@ -3,7 +3,7 @@ import Sidebar from "@/components/own/defaultSidebar";
 
 export default function Template({ children }: { children: React.ReactNode }) {
     return (
-        <div className="w-full flex flex-col h-screen">
+        <div className="w-full flex flex-col h-[100dvh]">
             <div className="w-screen h-16 flex justify-between fixed top-0 left-0 px-3 lg:px-8 py-4 bg-white dark:bg-[#020817] z-10">
                 <Navbar />
             </div>
