f9ebb9d04ceb38ed4caeb096493bf9489edd5453
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2024-01-11 00:22:43 +0700

    Update Navbar.tsx

--- components/own/Navbar.tsx
+++ components/own/Navbar.tsx
@@ -11,9 +11,8 @@ import {
 } from "react-icons/ai";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
-import { cn } from "@/lib/utils";
 import Image from "next/image";
-import { ChannelDataType } from "@/types/type";
+
 import axios from "axios";
 import {
     Tooltip,
@@ -37,8 +36,14 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import MenuItem from '@/components/own/navbar/MenuItem'
+import ModeSetting from "@/components/own/navbar/ModeSetting";
+const Notification = dynamic(() => import("@/components/own/navbar/Notification"));
+const SearchModule = dynamic(() => import("@/components/own/navbar/SearchModule"));
+const ChannelRender = dynamic(() => import("@/components/own/navbar/CheckChannel"));
 
-const Notification = dynamic(() => import("@/components/own/Notification"));
+
+
 export default function Navbar() {
     const { data: session, status } = useSession();
     const { setTheme } = useTheme();
@@ -53,7 +58,7 @@ export default function Navbar() {
     const popoverTriggerRef = useRef<HTMLDivElement>(null);
     const popoverContentRef = useRef<HTMLDivElement>(null);
 
-    const [personalChannelData, setPersonalChannelData] = useState<ChannelDataType>();
+
     const [mobileShowSearch, setMobileShowSearch] = useState<boolean>();
     const [showPopover, setShowPopover] = useState<{
         click: boolean;
@@ -90,80 +95,6 @@ export default function Navbar() {
         setMobileShowSearch(false);
     });
 
-    useEffect(() => {
-        if (session && session.user) {
-            axios
-                .get("/api/channel/data", {
-                    params: {
-                        accountId: session.user.id,
-                    },
-                })
-                .then((res) => {
-                    if (res.status == 200) {
-                        setPersonalChannelData(res.data);
-                    }
-                })
-                .catch((e) => {
-                    console.log(e);
-                });
-        }
-    }, [session]);
-
-
-
-    const Collapse = ({
-        trigger,
-        content,
-    }: {
-        trigger: React.ReactNode;
-        content: React.ReactNode;
-    }) => {
-        return (
-            <div className="flex flex-col gap-1 w-max group/box">
-                <div>{trigger}</div>
-                {<div className="hidden group-hover/box:block hover:bg-transparent hover:block">{content}</div>}
-            </div>
-        );
-    };
-
-    const lightModeSetting = useCallback(() => {
-        return (
-            <Collapse
-                trigger={
-                    <MenuItem className="text-start">Chế độ sáng</MenuItem>
-                }
-                content={
-                    <div className="flex flex-col w-full rounded-sm text-start">
-                        <button
-                            onClick={() => {
-                                setTheme("light");
-                            }}
-                            className="text-start py-1 pl-5 hover:bg-slate-200 hover:text-slate-800 rounded-md"
-                        >
-                            Sáng
-                        </button>
-                        <button
-                            onClick={() => {
-                                setTheme("dark");
-                            }}
-                            className="text-start py-1 pl-5 hover:bg-slate-600 rounded-md hover:text-white"
-                        >
-                            Tối
-                        </button>
-                        <button
-                            onClick={() => {
-                                setTheme("system");
-                            }}
-                            className="text-start py-1 pl-5 hover:bg-gradient-to-r from-slate-200 to-slate-600 rounded-md hover:text-white"
-                        >
-                            Hệ thống
-                        </button>
-                    </div>
-                }
-            />
-        );
-    }, []);
-
     const AccountAvatarRender = useCallback(() => {
         if (status == "loading") {
             return <Skeleton className="h-full w-full rounded-full" />;
@@ -192,59 +123,7 @@ export default function Navbar() {
         }
     }, [session]);
 
-    const ChannelRender = useCallback(() => {
-        if (!session && personalChannelData == undefined) {
-            return (
-                <MenuItem className="text-start">
-                    <Skeleton className="h-full w-full" />
-                </MenuItem>
-            );
-        } else if (session && personalChannelData == null) {
-            return (
-                <MenuItem
-                    onClick={() => {
-                        router.push("regchannel");
-                    }}
-                >
-                    Chưa có kênh? Tạo ngay!
-                </MenuItem>
-            );
-        } else if (session && personalChannelData) {
-            return (
-                <>
-                    <MenuItem className="bg-transparent">
-                        <div className="flex gap-4">
-                            <div className="flex items-center">
-                                <div className="relative w-8 h-8">
-                                    {personalChannelData.avatarImage && (
-                                        <Image
-                                            className="rounded-full"
-                                            src={
-                                                personalChannelData.avatarImage
-                                            }
-                                            alt=""
-                                            fill
-                                            sizes="1/1"
-                                        />
-                                    )}
-                                </div>
-                            </div>
-                            <div className="flex items-center">
-                                <p className="text-xl">{session.user.name}</p>
-                            </div>
-                        </div>
-                    </MenuItem>
-                    <MenuItem
-                        onClick={() => {
-                            router.push("station");
-                        }}
-                    >
-                        Station
-                    </MenuItem>
-                </>
-            );
-        }
-    }, [session, personalChannelData]);
+
 
     const DropMenu = useCallback(() => {
         return (
@@ -294,7 +173,7 @@ export default function Navbar() {
                                     >
                                         Cài đặt
                                     </MenuItem>
-                                    {lightModeSetting()}
+                                    <ModeSetting />
                                 </div>
                             ) : (
                                 <div className="flex flex-col gap-2 shadow-[0_0_5px_purple] p-3 rounded-lg bg-white dark:bg-[#020817]">
@@ -310,7 +189,7 @@ export default function Navbar() {
                                     >
                                         Cài đặt
                                     </MenuItem>
-                                    {lightModeSetting()}
+                                    <ModeSetting />
                                 </div>
                             )}
                         </div>
@@ -334,9 +213,7 @@ export default function Navbar() {
                             />
                         </button>
 
-                        <div className="relative flex gap-1 items-center rounded-2xl border-2 border-black">
-                            <SearchModule />
-                        </div>
+                        <SearchModule />
 
                         <div className="flex gap-3 items-center">
                             <AiOutlineUpload />
@@ -350,17 +227,16 @@ export default function Navbar() {
                 // device type: mobile, trạng thái: ẩn search input
                 return (
                     <>
-                        <div
-                            className="flex w-40 gap-8 text-xl items-center"
-                            onClick={() => {
+                        <div className="flex w-40 gap-8 text-xl items-center">
+                            <div onClick={() => {
                                 dispatch(reverse());
-                            }}
-                        >
-                            {openSidebar ? (
-                                <AiOutlineRight />
-                            ) : (
-                                <AiOutlineLeft />
-                            )}
+                            }}>
+                                {openSidebar ? (
+                                    <AiOutlineRight />
+                                ) : (
+                                    <AiOutlineLeft />
+                                )}
+                            </div>
                             <TooltipProvider>
                                 <Tooltip>
                                     <TooltipTrigger>
@@ -432,16 +308,14 @@ export default function Navbar() {
                             </Tooltip>
                         </TooltipProvider>
                     </div>
-                    <div className="relative flex gap-1 items-center rounded-2xl shadow-[0_0_2px_black] dark:bg-slate-800">
-                        <SearchModule />
-                    </div>
+                    <SearchModule />
                     <div className="flex gap-3 items-center">
                         <TooltipProvider>
                             <Tooltip>
                                 <TooltipTrigger>
                                     <DropdownMenu>
-                                        <DropdownMenuTrigger>
-                                            <div className="text-2xl cursor-pointer">
+                                        <DropdownMenuTrigger className="flex items-center">
+                                            <div className="text-2xl cursor-pointer flex items-center">
                                                 <AiOutlineUpload />
                                             </div>
                                         </DropdownMenuTrigger>
@@ -502,10 +376,9 @@ export default function Navbar() {
     }
 }
 
-const MenuRender = memo(
-    function Comp(){
+const MenuRender = () => {
     return (
-        <Link href={"/"} className="text-xl flex items-center gap-2">
+        <Link href={"/"} className="text-xl flex items-center gap-2" >
             <div className="relative w-5 h-5 lg:w-9 lg:h-9">
                 <Image
                     className="rounded-full animate-spin"
@@ -517,79 +390,10 @@ const MenuRender = memo(
                     alt="khá bảnh"
                 />
             </div>
-        </Link>
+        </Link >
     );
-});
+}
 
-const MenuItem = ({
-    children,
-    className,
-    ...props
-}: React.HTMLProps<HTMLDivElement>) => {
-    return (
-        <div
-            className={cn(
-                "w-full px-3 py-3 rounded-sm cursor-pointer hover:bg-slate-300 dark:hover:bg-slate-700",
-                className
-            )}
-            {...props}
-        >
-            <p className="w-full min-w-[160px] select-none">{children}</p>
-        </div>
-    );
-};
 
-const SearchModule = () => {
-    const router = useRouter();
-    const searchInputRef = useRef<HTMLInputElement>(null);
-    const searchResultRef = useRef<HTMLDivElement>(null);
-    const [searchValue, setSearchValue] = useState<string>("");
-    const [focusing, setFocus] = useState<boolean>(false);
-    return (
-        <>
-            <div className="w-max flex items-center h-full">
-                <input
-                    className="bg-transparent pr-1 w-60 max-sm:w-40 focus:outline-none ml-3 my-1"
-                    ref={searchInputRef}
-                    value={searchValue}
-                    onChange={(e) => {
-                        setSearchValue(e.target.value);
-                    }}
-                    onFocus={() => {
-                        setFocus(true);
-                    }}
-                    onBlur={() => setFocus(false)}
-                />
-                <div className="w-[2px] h-full relative after:absolute after:bg-slate-300 dark:after:bg-slate-500 after:h-[90%] after:top-[5%] after:left-0 after:w-full" />
-                <div
-                    className="h-full w-8 flex flex-col pl-2 justify-center cursor-pointer hover:bg-slate-400 dark:hover:bg-slate-700 text-[#020817] dark:text-[white] rounded-r-2xl"
-                    onClick={() => {
-                        router.push(`/result/${searchValue}`);
-                    }}
-                >
-                    <AiOutlineSearch />
-                </div>
-            </div>
 
-            {focusing ? (
-                <div
-                    className="absolute w-80 h-fit left-[-16px] items-center top-12 max-w-[95vw] bg-white dark:bg-slate-600 border-[1px] border-slate-400 rounded-lg"
-                    ref={searchResultRef}
-                >
-                    <div className="flex px-4 py-1 gap-9 h-max items-center">
-                        <div className="flex items-center">
-                            <AiOutlineSearch />
-                        </div>
-                        <div className="flex items-center flex-1 h-fit">
-                            <p className="w-full text-lg whitespace-nowrap text-ellipsis h-max overflow-hidden">
-                                {searchValue}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            ) : (
-                <></>
-            )}
-        </>
-    );
-};
+
