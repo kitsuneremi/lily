e49176812ad68e1cd2e30371943f508db99da015
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2024-01-14 17:51:04 +0700

    Update current-channel-slice.ts

--- redux/features/current-channel-slice.ts
+++ redux/features/current-channel-slice.ts
@@ -2,38 +2,39 @@ import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import type { ChannelDataType } from '@/types/type';
 
 interface ChannelDataResponse {
-  channelData: ChannelDataType;
+    channelData: ChannelDataType;
 }
 
 export const fetchChannelData = createAsyncThunk('channelData/fetchData', async (id: number) => {
-  const response = await fetch(`https://erinasaiyukii.com/api/channel/data?accountId=${id}`);
-  const data: ChannelDataResponse = await response.json();
-  return data.channelData;
+    const response = await fetch(`https://erinasaiyukii.com/api/channel/data?accountId=${id}`);
+    const data: ChannelDataResponse = await response.json();
+    return data.channelData;
 });
 
 type Init = {
-  value: {
-    channelData: ChannelDataType | null;
-  };
+    value: {
+        channelData: ChannelDataType | null;
+    };
 };
 
 export const channelData = createSlice({
-  name: 'channelData',
-  initialState: {
-    value: {
-      channelData: null,
+    name: 'channelData',
+    initialState: {
+        value: {
+            channelData: null,
+        },
+    } as Init,
+    reducers: {
+        set: (state, action: PayloadAction<Init>) => {
+            state.value.channelData = action.payload.value.channelData;
+        },
     },
-  } as Init,
-  reducers: {
-    set: (state, action: PayloadAction<Init>) => {
-      state.value.channelData = action.payload.value.channelData;
+    extraReducers: (builder) => {
+        builder.addCase(fetchChannelData.fulfilled, (state, action) => {
+            console.log('slice:' + action.payload);
+            state.value.channelData = action.payload;
+        });
     },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(fetchChannelData.fulfilled, (state, action) => {
-      state.value.channelData = action.payload;
-    });
-  },
 });
 
 export default channelData.reducer;
