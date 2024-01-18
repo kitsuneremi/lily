ef2da2c31b5b06b07d606552b97a9e665b0069c5
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2024-01-18 20:34:08 +0700

    Update Navbar.tsx

--- components/own/Navbar.tsx
+++ components/own/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useMediaQuery, useEffectOnce } from "usehooks-ts";
 import React, { useState } from "react";
-import { BsChatLeftDots } from "react-icons/bs";
 import {
     AiOutlineClose,
     AiOutlineRight,
@@ -10,7 +9,6 @@ import {
     AiOutlineSearch,
 } from "react-icons/ai";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
 import Image from "next/image";
 import {
     Tooltip,
@@ -18,20 +16,11 @@ import {
     TooltipProvider,
     TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { useTheme } from "next-themes";
 import { useSsr } from "usehooks-ts";
 import dynamic from "next/dynamic";
 import { useDispatch } from "react-redux";
 import { useAppSelector } from "@/redux/storage";
-import { close, reverse, open } from "@/redux/features/sidebar-slice";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { close, reverse } from "@/redux/features/sidebar-slice";
 import MessageBox from "./navbar/Message";
 
 const Notification = dynamic(() => import("@/components/own/navbar/Notification"));
@@ -40,8 +29,6 @@ const LastMenu = dynamic(() => import("@/components/own/navbar/LastMenu"));
 
 
 export default function Navbar() {
-    const { isBrowser } = useSsr();
-    
     const dispatch = useDispatch();
 
     const openSidebar = useAppSelector(
@@ -61,34 +48,14 @@ export default function Navbar() {
         setMobileShowSearch(false);
     });
 
-    
-    if (isBrowser) {
-        if (deviceType.isMobile) {
-            return (
-                <>
-                    {mobileShowSearch ? <div className="flex w-40 gap-8 text-xl items-center">
-                        <div onClick={() => {
-                            dispatch(reverse());
-                        }}>
-                            {openSidebar ? (
-                                <AiOutlineRight />
-                            ) : (
-                                <AiOutlineLeft />
-                            )}
-                        </div>
-                        <TooltipProvider>
-                            <Tooltip>
-                                <TooltipTrigger>
-                                    <MenuRender />
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                    <p>Quay về trang chủ</p>
-                                </TooltipContent>
-                            </Tooltip>
-                        </TooltipProvider>
-                    </div>
-                        :
-                        <>
+    return (
+        <>
+            <div className="flex gap-8 select-none items-center">
+                {
+                    deviceType.isMobile
+                        ?
+                        mobileShowSearch
+                            ?
                             <button className="text-xl ml-3">
                                 <AiOutlineClose
                                     onClick={() => {
@@ -96,126 +63,68 @@ export default function Navbar() {
                                     }}
                                 />
                             </button>
-                            <SearchModule />
-                        </>
-
-                    }
-
-
-                    <div className="flex gap-3 items-center">
-                        {!mobileShowSearch && <div
-                            className="lg:hidden"
-                            onClick={() => {
-                                setMobileShowSearch(true);
-                            }}
-                        >
-                            <AiOutlineSearch />
-                        </div>}
-                        <AiOutlineUpload />
-                        <Notification />
-                        <MessageBox />
-                        <LastMenu />
-                    </div>
-                </>
-            )
-        } else {
-            return (
-                // device type: desktop hoặc tablet
-                <>
-                    <div className="w-40 flex gap-8 select-none">
-                        <div
-                            className="text-xl flex items-center cursor-pointer"
-                            onClick={() => {
-                                dispatch(reverse());
-                            }}
-                        >
+                            :
+                            <div onClick={() => { dispatch(reverse()); }}>
+                                {openSidebar ? (
+                                    <AiOutlineRight />
+                                ) : (
+                                    <AiOutlineLeft />
+                                )}
+                            </div>
+                        :
+                        <div onClick={() => { dispatch(reverse()); }}>
                             {openSidebar ? (
                                 <AiOutlineRight />
                             ) : (
                                 <AiOutlineLeft />
                             )}
                         </div>
-                        <TooltipProvider>
-                            <Tooltip>
-                                <TooltipTrigger>
-                                    <Link
-                                        href={"/"}
-                                        className="text-xl flex items-center gap-2"
-                                    >
-                                        <div className="relative w-5 h-5 lg:w-9 lg:h-9">
-                                            <Image
-                                                className="rounded-full animate-spin"
-                                                src={
-                                                    "https://danviet.mediacdn.vn/upload/2-2019/images/2019-04-02/Vi-sao-Kha-Banh-tro-thanh-hien-tuong-dinh-dam-tren-mang-xa-hoi-khabanh-1554192528-width660height597.jpg"
-                                                }
-                                                fill
-                                                sizes="1/1"
-                                                alt="khá bảnh"
-                                            />
-                                        </div>
-                                    </Link>
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                    <p>Quay về trang chủ</p>
-                                </TooltipContent>
-                            </Tooltip>
-                        </TooltipProvider>
-                    </div>
-                    <SearchModule />
-                    <div className="flex gap-3 items-center">
-                        <TooltipProvider>
-                            <Tooltip>
-                                <TooltipTrigger>
-                                    <DropdownMenu>
-                                        <DropdownMenuTrigger className="flex items-center">
-                                            <div className="text-2xl cursor-pointer flex items-center">
-                                                <AiOutlineUpload />
-                                            </div>
-                                        </DropdownMenuTrigger>
-                                        <DropdownMenuContent>
-                                            <DropdownMenuLabel>
-                                                Loại nội dung đăng tải
-                                            </DropdownMenuLabel>
-                                            <DropdownMenuSeparator />
-                                            <DropdownMenuItem>
-                                                <Link
-                                                    href={
-                                                        "/station/upload/video"
-                                                    }
-                                                    className="w-full"
-                                                >
-                                                    Video
-                                                </Link>
-                                            </DropdownMenuItem>
-                                            <DropdownMenuItem>
-                                                <Link
-                                                    href={
-                                                        "/station/upload/livestream"
-                                                    }
-                                                    className="w-full"
-                                                >
-                                                    Sự kiện trực tiếp
-                                                </Link>
-                                            </DropdownMenuItem>
-                                        </DropdownMenuContent>
-                                    </DropdownMenu>
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                    <p>Đăng tải nội dung</p>
-                                </TooltipContent>
-                            </Tooltip>
-                        </TooltipProvider>
-                        <Notification />
-                        <MessageBox />
-                        <LastMenu />
-                    </div>
-                </>
-            );
-        }
-    }
+
+
+                }
+                {
+                    (
+                        !deviceType.isMobile
+                        &&
+                        !mobileShowSearch
+                    )
+                    &&
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <MenuRender />
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>Quay về trang chủ</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                }
+            </div>
+
+
+
+            {(!deviceType.isMobile || (deviceType.isMobile && mobileShowSearch)) && <SearchModule />}
+            <div className="flex gap-3 items-center">
+                {!mobileShowSearch && <div
+                    className="lg:hidden"
+                    onClick={() => {
+                        setMobileShowSearch(true);
+                    }}
+                >
+                    <AiOutlineSearch />
+                </div>}
+                <AiOutlineUpload />
+                <Notification />
+                <MessageBox />
+                <LastMenu />
+            </div>
+
+        </>
+    )
 }
 
-const MenuRender = ():React.ReactNode => {
+const MenuRender = (): React.ReactNode => {
     return (
         <Link href={"/"} className="text-xl flex items-center gap-2" >
             <div className="relative w-5 h-5 lg:w-9 lg:h-9">
