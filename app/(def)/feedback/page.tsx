3c442a29d72e898f715af147d8e39786e2d8b58e
lily <89727355+kitsuneremi@users.noreply.github.com>
2024-02-16 21:05:01 +0700

    16/2

--- app/(def)/feedback/page.tsx
+++ app/(def)/feedback/page.tsx
@@ -1,4 +1,14 @@
-export default function Page(){
+'use client'
+import { useToast } from '@/components/ui/use-toast'
+import Image from 'next/image';
+export default function Page() {
+    const { toast } = useToast();
+
+    const handlePost = () => {
+        toast({
+            title: 'ý kiến của bạn đã được ghi nhận'
+        })
+    }
 
     return (
         <div className="w-full h-full flex justify-center items-center">
@@ -7,8 +17,8 @@ export default function Page(){
 
                 </textarea>
                 <div className="flex gap-5 h-max justify-around">
-                    <img src='https://i.imgflip.com/5rh6bx.png' alt='' className="w-28 h-28 rounded-lg"/>
-                    <div className="h-28 flex items-center"><button className='px-3 py-1 rounded-lg bg-cyan-500'>Đóng góp ý kiến của bạn</button></div>
+                    <Image src='https://i.imgflip.com/5rh6bx.png' alt='' width={160} height={160} className="rounded-lg" />
+                    <div className="h-28 flex items-center"><button className='px-3 py-1 rounded-lg bg-cyan-500' onClick={handlePost}>Đóng góp ý kiến của bạn</button></div>
                 </div>
             </div>
         </div>
--- app/api/userinfo/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req:NextRequest, res:NextResponse){
-    console.log(req.body);
-
-    return new Response(JSON.stringify({'message': req}))
-}
