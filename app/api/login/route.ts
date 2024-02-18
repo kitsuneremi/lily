e8e73dac2aac94d5db475c0e4a05c0b21a577f60
lily <89727355+kitsuneremi@users.noreply.github.com>
2024-02-19 03:34:30 +0700

    Update route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -7,27 +7,28 @@ interface RequestBody {
 }
 
 export async function POST(request: Request) {
-    const body = await request.json();
+    return new Response(JSON.stringify(null));
+    // const body = await request.json();
 
-    if (body && body.username) {
-        const user = await prisma.accounts.findFirst({
-            where: {
-                username: body.username,
+    // if (body && body.username) {
+    //     const user = await prisma.accounts.findFirst({
+    //         where: {
+    //             username: body.username,
 
-            }
-        })
+    //         }
+    //     })
 
-        if (user && body.password == user.password) {
-            const { password, ...UserWithoutPass } = user
-            const accessToken = signJWTToken(UserWithoutPass)
-            const result = {
-                ...UserWithoutPass,
-                accessToken
-            }
-            return new Response(JSON.stringify(result))
-        } else {
-            return new Response(JSON.stringify(null));
-        }
-    }
+    //     if (user && body.password == user.password) {
+    //         const { password, ...UserWithoutPass } = user
+    //         const accessToken = signJWTToken(UserWithoutPass)
+    //         const result = {
+    //             ...UserWithoutPass,
+    //             accessToken
+    //         }
+    //         return new Response(JSON.stringify(result))
+    //     } else {
+    //         return new Response(JSON.stringify(null));
+    //     }
+    // }
 }
 
