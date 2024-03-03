8e14f458d9282b72867fd5aa86ce130fbcf797ec
lily <89727355+kitsuneremi@users.noreply.github.com>
2024-03-03 12:14:43 -0800

    Update page.tsx

--- app/(stream)/stream/[tag]/no-stream/page.tsx
+++ app/(stream)/stream/[tag]/no-stream/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function Page({ params, searchParams }: Props) {
 
     return <div className="flex w-screen justify-center items-center">
-        <div className="bg-slate-700 glow">
+        <div className="bg-slate-700 glow px-2 py-1">
             <p className="font-bold text-lg hover:scale-105">@{params.tag} chưa livestream, quay lại sau nhé</p>
         </div>
     </div>
